1575a94dcf85745ae8022441091ff180e4ad3d2f
Anmol Gupta <gupta010anmol@gmail.com>
2022-11-08 12:58:12 +0530

    before npm run build and front page done

--- src/axios/Request.jsx
+++ src/axios/Request.jsx
@@ -3,12 +3,12 @@ const API_KEY = "410829d0794e6d88f1c40cbae56a9f50";
 const requests = {
   fetchTrending: `/trending/all/day?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/movie?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_gernres=28`,
-  fetchCommedyMovies: `/discover/movie?api_key=${API_KEY}&with_gernres=35`,
-  fetchHorroMovies: `/discover/movie?api_key=${API_KEY}&with_gernres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_gernres=10749`,
-  fetchDocumentries: `/discover/movie?api_key=${API_KEY}&with_gernres=99`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_genres=28`,
+  fetchCommedyMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_genres=35`,
+  fetchHorroMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_genres=10749`,
+  fetchDocumentries: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_genres=99`,
 };
 
 export default requests;
